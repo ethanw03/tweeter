3a75519e478112cd6535ca0fdb9530236d4a4e16
Ethan Wong <ethanwong98@yahoo.ca>
2023-02-09 12:26:21 -0800

    corrections

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,72 +1,71 @@
-$(document).ready(function () {
-  const loadTweets = function () {
-    $.ajax("/tweets").then(function (response) {
+$(document).ready(function() {
+  const loadTweets = function() {
+    $.ajax("/tweets").then(function(response) {
       renderTweets(response);
     });
   };
   loadTweets();
   $(".error").hide();
 
-  $("#tweetForm").on("submit", function(event) {
-    event.preventDefault();
-    $.ajax("/tweets", {
-      method: "POST",
-      data: $(this).serialize()
-    })
-    console.log(event);
-  })
-
+  //takes tweets input as parameter and renders
   const renderTweets = function(tweets) {
     $("#tweets-container").empty();
     for (let item of tweets) {
       const tweetElement = createTweetElement(item);
-     $("#tweets-container").prepend(tweetElement);
-     }
-  }
+      $("#tweets-container").prepend(tweetElement);
+    }
+  };
 
-  const escape = function (str) {
+  //function to stop cross-site scripting, takes string inputted as parameter
+  const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
-  }
+  };
 
+  //creates HTML for new tweets in tweetbox with heart, retweet, and like buttons
   const createTweetElement = function(tweet) {
     const $tweeter = `
       <article class="tweet-container">
         <header class="header2">
-          <div class="topLeft"> <img src="${tweet.user.avatars}"> ${tweet.user.name}</div>
+          <div class="topLeft"> <img src="${tweet.user.avatars}"> ${
+  tweet.user.name
+        }</div>
           ${tweet.user.handle}
         </header>
-        <main class="bigBody">
-          <span class="theTweet"
-            > ${tweet.content.text}</span
+        <main class="tweet-body">
+          <span class="tweet-text"
+            > ${escape(tweet.content.text)}</span
           >
           <hr />
         </main>
         <footer class="bottom">
           ${timeago.format(tweet.created_at)}
           <div class="icons">
-            <i id="flag" class="fa-solid fa-flag"></i>
+            <i id="flag" class="fa-solid fa-flag"></i> 
             <i id="retweet" class="fa-solid fa-retweet"></i>
             <i id="like" class="fa-solid fa-heart"></i>
           </div>
         </footer>
-      </article> `
+      </article> `;
     return $tweeter;
-  }
-  $("#tweetForm").on("submit", function (event) {
+  };
+
+  //submits new tweets as well as handles user input parameters
+  $("#tweetForm").on("submit", function(event) {
     event.preventDefault();
-    const textBox = $("#tweet-text").val().trim().length;
-    if (!textBox) {
+    const tweetLength = $("#tweet-text").val().trim().length;
+    const charLimit = 140
+    if (!tweetLength) {
       $(".error").text("Tweet box cannot be empty");
-      $(".error").slideDown("slow")
+      $(".error").slideDown("slow");
       $(".error").delay(3000).slideUp("slow");
 
       return;
     }
-    if (textBox > 140) {
-      $(".error").text("Tweet cannot be more than 140 characters‼️");
-      $(".error").slideDown("slow")
+    if (tweetLength > charLimit) {
+      $(".error").text("Tweet cannot be more than charLimit characters‼️");
+      $(".error").slideDown("slow");
       $(".error").delay(3000).slideUp("slow");
 
       return;
@@ -77,7 +76,7 @@ $(document).ready(function () {
     }).then(() => {
       loadTweets();
       $("#tweet-text").val("");
-      $(".counter").text(140);
+      $(".counter").text(charLimit);
     });
   });
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
   $("#charOverLimit").hide();
   $("#inputEmpty").hide();
-  $("output").css({'color': 'black'});
+  $("output").css({ color: "black" });
 
-  $("textarea").on('input', function() {
+  //function to count characters. goes red if under 0
+  const charLimit = 140
+  $("textarea").on("input", function() {
     const textCounter = $(this).closest("section").find("output")[0];
     const tweetTextCount = $(this).val().length;
-    textCounter.innerHTML = 140 - tweetTextCount;
-
-    if (textCounter.innerHTML <= 0) {
-      $("#charOverLimit").show(); 
-      $("output").css({'color': 'red'}); 
+    textCounter.innerHTML = charLimit - tweetTextCount;
+    if (textCounter.innerHTML < 0) {
+      $("#charOverLimit").show();
+      $("output").css({ color: "red" });
     } else {
-      $("output").css({'color': 'black'});
+      $("output").css({ color: "black" });
       $("#charOverLimit").hide();
-
     }
   });
 });
